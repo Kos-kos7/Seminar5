9ec707383d709a76071d6840fe6dd16f44a3f369
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-25 14:48:03 +0500

    разница между макс и мин в массиве

---
+++ Task_38(HomeWork)/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив вещественных чисел. Найдите
+// разницу между максимальным и минимальным
+// элементов массива.
+
+int[] nums = new int[5];
+var numRnd = new Random();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(0, 100);
+    Console.Write(nums[i] + " ");
+    
+}
+
+int max = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+   if (nums[i] > max)
+   {
+       max = nums[i];
+   }
+}
+Console.WriteLine("Максимальный элемент массива: " + max);
+
+int min = nums[0];
+for (int i = 0; i < nums.Length; i++)
+{
+   if (nums[i] < min)
+   {
+       min = nums[i];
+   }
+}
+Console.WriteLine("Минимальный элемент массива: " + min);
+
+int dif = max - min;
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива: " + dif);
