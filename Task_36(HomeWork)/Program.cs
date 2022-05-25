83d1e2fa5a0c36fc83264a87b58b2b5f1f2096d7
Konstantin Andreev <konstantin-andr@mail.ru>
2022-05-25 14:01:15 +0500

    Сумма нечетных элементов массива

---
+++ Task_36(HomeWork)/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+
+int[] nums = new int[5];
+var numRnd = new Random();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = numRnd.Next(0, 100);
+    Console.Write(nums[i] + " ");
+}
+
+int sum = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+   if (i % 2 != 0)
+   {
+       sum = sum + nums[i];
+   }
+   
+}
+
+Console.WriteLine("Сумма элементов, стоящих на нечётных позициях: " + sum);
