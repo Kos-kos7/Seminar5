a0ec270460e5faf99a5ee8e8d7c3b6c0d9613286
Konstantin Andreev <konstantin-andr@mail.ru>
2022-06-04 09:52:21 +0500

    в двоичную через рекурсию

---
+++ Task42.1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите число в десятичной системе для перевода в двоичную: ");
+int numDec = int.Parse(Console.ReadLine());
+Console.Write("Введите основание системы счисления: ");
+int baseNum = int.Parse(Console.ReadLine());
+
+// int d10 = 1;
+// long result = 0;
+
+// while (numDec != 0)
+// {
+//     result = result + numDec % baseNum * d10;
+//     numDec /= baseNum; //numDec = numDec / 2
+//     d10 = d10 * 10;
+// }
+
+// Console.WriteLine(result);
+
+
+void DecToOther (int num, int baseN)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    DecToOther (num / baseN, baseN);
+
+    Console.Write(num%baseN);
+}
+
+DecToOther (numDec, baseNum);
+
