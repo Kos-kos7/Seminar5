d7e2eae13c3c146e5654610cadfb87e2106e5721
Konstantin Andreev <konstantin-andr@mail.ru>
2022-06-04 21:33:28 +0500

    Задать двумерный массив m x n

---
+++ Task_47(HomeWork)/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int[,] GenArray()
+{
+    Console.Write("Введите число строк массива m: ");
+    int m = int.Parse(Console.ReadLine ());
+    Console.Write("Введите число столбцов массива n: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[,] arr = new int[m, n];
+    Random rand = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rand.Next(11);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] prarr)
+{
+    for (int i = 0; i < prarr.GetLength(0); i++)
+    {
+        for (int j = 0; j < prarr.GetLength(1); j++)
+        {
+            Console.Write(prarr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GenArray();
+PrintArray(array);
+Console.WriteLine();
