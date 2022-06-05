a1aa4bd22c5fb6a5f50950d84ba07ee21d65ea0b
Konstantin Andreev <konstantin-andr@mail.ru>
2022-06-05 10:08:48 +0500

    массив с эелементом как сумма индексов

---
+++ Task_48/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] GenArray()
+{
+    Console.Write("Введите число строк массива m: ");
+    int m = int.Parse(Console.ReadLine ());
+    Console.Write("Введите число столбцов массива n: ");
+    int n = int.Parse(Console.ReadLine ());
+    int[,] arr = new int[m, n];
+    Random rand = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] prarr)
+{
+    for (int i = 0; i < prarr.GetLength(0); i++)
+    {
+        for (int j = 0; j < prarr.GetLength(1); j++)
+        {
+            Console.Write(prarr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] array = GenArray();
+PrintArray(array);
+Console.WriteLine();
